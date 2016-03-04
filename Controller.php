c77198403df32dac866c635bd6496958be2cced5
Pongpayak Handsome <donus33@gmail.com>
2016-03-04 21:10:36 +0700

    semi-final edit Controller

--- Controller.php
+++ Controller.php
@@ -12,7 +12,7 @@
 				foreach ($topics['rows'] as $topic) {
 					$field = $topic['doc'];
 					if($field['_id'] === $id){
-						return $topic;
+						return $field;
 					}
 				}
 			}
@@ -34,7 +34,7 @@
 			}
 		}
 
-		public function getWeater($la,$lo,$date,$tag){
+		public function getWeather($la,$lo,$tag){
 			$weatherModel = new WeatherModel;
 			$json = file_get_contents('https://ad46647d-1b77-4644-b8c6-78eed562b7a3:NAgf7Zkmlo@twcservice.au-syd.mybluemix.net:443/api/weather/v2/forecast/daily/10day?units=m&geocode=45.42%2C75.69&language=en-US');
 			$obj = json_decode($json);
@@ -50,8 +50,8 @@
 			$tn = 0;
 			// count stat of forcast each day
 			foreach ($forcasts as $forcast) {
-				$day = $day->{'day'} ;
-				$night = $night->{'night'};
+				$day = $forcast->{'day'} ;
+				$night = $forcast->{'night'};
 				 if($day != NULL){
 					 if(strpos($day->{'shortcast'}, 'sun' ) !== false || strpos($day->{'shortcast'}, 'Sun' ) !== false)
 					 		$s++;
@@ -83,19 +83,17 @@
 			foreach ($rain_day as $value) {
 				$rain_day_print = $rain_day_print . " " . $value;
 			}
-			array_push($result, "day");
-			array_push($result,'"percent"'. $s .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","sun").'}');
-			array_push($result,'"percent"'. $c .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","clear").'}');
-			array_push($result,'"percent"'. $t .',"date":'. $rain_day_print .',"description" : ' . $weatherModel->getWeatherByKeys($tag,"day","rain").'}');
+			array_push($result,'{"period":"day","percent"'. $s .',"date":null,"description" : "' . $weatherModel->getWeatherByKeys($tag,"day","sun").'"}');
+			array_push($result,'{"period":"day","percent"'. $c .',"date":null,"description" : "' . $weatherModel->getWeatherByKeys($tag,"day","clear").'"}');
+			array_push($result,'{"period":"day","percent"'. $t .',"date":"'. $rain_day_print .'","description" : "' . $weatherModel->getWeatherByKeys($tag,"day","rain").'"}');
 			//night
 			$rain_night_print = "";
 			foreach ($rain_night as $value) {
 				$rain_night_print = $rain_night_print . " " . $value;
 			}
-			array_push($result, "night");
-			array_push($result,'"percent"'. $sn .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","sun").'}');
-			array_push($result,'"percent"'. $cn .',"date":null,"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","clear").'}');
-			array_push($result,'"percent"'. $tn .',"date":'. $rain_night_print .',"description" : ' . $weatherModel->getWeatherByKeys($tag,"night","rain").'}');
+			array_push($result,'{"period":"night","percent"'. $sn .',"date":null,"description" : "' . $weatherModel->getWeatherByKeys($tag,"night","sun").'"}');
+			array_push($result,'{"period":"night","percent"'. $cn .',"date":null,"description" : "' . $weatherModel->getWeatherByKeys($tag,"night","clear").'"}');
+			array_push($result,'{"period":"night","percent"'. $tn .',"date":"'. $rain_night_print .'","description" : "' . $weatherModel->getWeatherByKeys($tag,"night","rain").'"}');
 
 			return json_encode($result);
 		}
